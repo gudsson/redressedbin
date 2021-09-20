c38a884b4741c32cdef0065ed6cdf4e07fc9d2a4
Jay Gudsson <gudsson@gmail.com>
2021-09-20 11:47:37 -0600

    prints request data

--- controllers/bins.js
+++ controllers/bins.js
@@ -3,8 +3,13 @@ const Bin = require('../models/bin')
 
 binsRouter.all('/:id', (req, res, next) => {
   const binId = req.params.id
+  let body = req.body
+  let headers = req.headers
+  let method = req.method
+  
   console.log(req)
-  res.json({'msg': `bin ${binId} has been hit`})
+  res.json({ method, headers, body })
+  // res.json({'msg': `bin ${binId} has been hit`})
 })
 
 module.exports = binsRouter
