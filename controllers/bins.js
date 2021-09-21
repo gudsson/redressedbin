5252be91a1c14584c7af3063c81f43cd3cca2649
Jay Gudsson <gudsson@gmail.com>
2021-09-21 13:35:15 -0600

    still testing

--- controllers/bins.js
+++ controllers/bins.js
@@ -42,7 +42,7 @@ binsRouter.get('/inspect/:id', (req, res, next) => {
 
 binsRouter.all('/:id', (req, res, next) => {
   const binId = req.params.id
-  console.log(req.method)
+  console.log(req.method) // testing
   let reqObj = {
     originalUrl: req.originalUrl,
     baseUrl: req.baseUrl,
